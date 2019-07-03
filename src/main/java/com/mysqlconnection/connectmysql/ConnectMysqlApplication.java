8ed5cfb56966c801f8267290d9a33e9013504280
immad.hamid <immad.hamid@gmail.com>
2019-07-03 11:45:24 +0500

    # Change Log: - Implemented Crud - Connected with MySQL - Logging results in files - 3rd Party API usage and returning it with own end-point

---
+++ src/main/java/com/mysqlconnection/connectmysql/ConnectMysqlApplication.java
@@ -0,0 +1,26 @@
+package com.mysqlconnection.connectmysql;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConnectMysqlApplication {
+    private static final Logger log = LoggerFactory.getLogger(ConnectMysqlApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectMysqlApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
